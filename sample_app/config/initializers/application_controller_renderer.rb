ea4da91cc8b688c627373a44dd773d87d42b3482
infra-bbb <mitsuya.tomoaki.infratop@gmail.com>
2019-09-15 23:37:04 +0900

    [Finish] 3-9

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '67c9d3f956338d5b4090b6dfa370928f8539aac3c32e1cd8f4f8de49689f3e37a69fb3d56f18ae66a565a5b10ecdb7312b8fee270152612b641189e9d36b093c'
